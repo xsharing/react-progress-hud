a97ddcc8fc9ae525fea2f4501eb3e46d6a7295e2
Nobuyuki Horiuchi <horiuchinobuyuki@gmail.com>
2019-04-16 18:56:51 +0900

    hmm..

--- src/components/progress-hud.js
+++ src/components/progress-hud.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { Spring } from "react-motion";
 
 import styles from "../styles";
